08e6950b6f6571a3698a7c66842e5392df21917f
J. Hench <49944492+juhench@users.noreply.github.com>
2024-04-01 13:35:21 +0200

    Add files via upload

--- install_voktrain.sh
+++ install_voktrain.sh
@@ -2,8 +2,6 @@
 
 # installation script for voktrain
 
-sudo apt -y update
-sudo apt -y upgrade
 sudo apt install -y python3 python3-pip python3-venv build-essential
 python3 -m venv voktrain-venv
 source ./voktrain-venv/bin/activate
